605e44b1eb5883a2ce8b1eb1b2a5d3e802bbc01f
QWYNG <ikusawasi@gmail.com>
2018-03-15 23:47:36 +0900

    get macth details

--- note/models.py
+++ note/models.py
@@ -12,6 +12,13 @@ class SNset():
         my_region = 'jp1'
         me = watcher.summoner.by_name(my_region, summoner_name_data)
         my_ranked_stats  = watcher.league.positions_by_summoner(my_region, me['id'])
+        recentmatchlists = watcher.match.matchlist_by_account_recent(my_region,me[ 'accountId'])
+        matches = recentmatchlists['matches']
+        x = 0
+        match_detail = []
+        for x in range(20):
+            match_detail.append(watcher.match.by_id(my_region, matches[x]['gameId']))
+            x += 1
         for my_ranked_stat in my_ranked_stats:
-            dict = {'my_ranked_stat': my_ranked_stat, 'me': me}
+            dict = {'my_ranked_stat': my_ranked_stat, 'me': me,"recentmatchlists":recentmatchlists,'match_detail':match_detail}
         return (dict)
